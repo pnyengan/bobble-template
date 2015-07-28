fd712c636a21af4c1b8192a62c002d237acac81e
Phikolomzi Nyengane <phikolomzinyengane@gmail.com>
2015-07-28 15:59:41 +0200

    adding urls

--- clock.rb
+++ clock.rb
@@ -3,6 +3,6 @@ require 'net/http'
 require 'bobble'
 
 module Clockwork
-  every(2.minutes, 'monitor-failure') { Bobble.check("http://broken.example") }
-  every(2.minutes, 'monitor-success') { Bobble.check("http://www.iana.org/") }
+  every(2.minutes, 'monitor-failure') { Bobble.check("http://imqsrc/") }
+  every(2.minutes, 'monitor-success') { Bobble.check("http://imqsrc/") }
 end
